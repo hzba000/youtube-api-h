fc584f0533392326fc4dd0f4099b63a739a2e346
Hozefa Bharmal <hozefa.bharmal@gmail.com>
2018-08-28 19:06:31 -0700

    I have made this more a11y friendly by including aria-live regions and also providing total number of search results

--- index.js
+++ index.js
@@ -10,26 +10,29 @@ function getDataFromApi(searchTerm, callback) {
   $.getJSON(YOUTUBE_SEARCH_URL, query, callback);
 }
 
-function renderResult(result) {
+function renderResult(item) {
   return `
     <div class="result-container">
 
         <div class="pic-container">
-            <p> <a href = "https://www.youtube.com/watch?v=${result.id.videoId}"><img src= "${result.snippet.thumbnails.medium.url}" alt="thumbnail"></a> </p>
+            <p> <a href = "https://www.youtube.com/watch?v=${item.id.videoId}"><img src= "${item.snippet.thumbnails.medium.url}" alt="thumbnail"></a> </p>
         </div>
 
         <div class="description-container">
-            <p class="result-title"><a href = "https://www.youtube.com/watch?v=${result.id.videoId}"> ${result.snippet.title}</a> </p>
-            <p class="result-description"> ${result.snippet.description} </p>
-            <p> Looking for more? Check out this <a href = "https://www.youtube.com/channel/${result.snippet.channelId}">channel</a> 
+            <p class="result-title"><a href = "https://www.youtube.com/watch?v=${item.id.videoId}"> ${item.snippet.title}</a> </p>
+            <p class="result-description"> ${item.snippet.description} </p>
+            <p> Looking for more? Check out this <a href = "https://www.youtube.com/channel/${item.snippet.channelId}">channel</a> 
         </div>
     </div>`
 }
 
 function displayYoutubeSearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item));
-  $('.js-search-results').html(results);
-  
+  const items = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(items);
+  console.log(data.pageInfo.totalResults);
+  console.log(data.nextPageToken);
+  // alert(`${data.nextPageToken}`);
+  $('.result-number').append(data.pageInfo.totalResults);
 }
 
 function watchSubmit() {
@@ -40,7 +43,7 @@ function watchSubmit() {
     // clear out the input
     queryTarget.val("");
     getDataFromApi(query, displayYoutubeSearchData);
-
+    $('.result-header, .result-number').removeClass('hidden');
   });
 }
 
